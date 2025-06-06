bf622c68479906052d46e3e611ee74679f5cd52f
MohammadarefQoliof <mhmmdarifquliyev33@gmail.com>
2025-06-06 10:36:59 +0400

    py

--- Battery check.py
+++ Battery check.py
@@ -1,15 +1,17 @@
 import time
 import subprocess
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
 
+percentage = simpledialog.askinteger("Percentage", "When do you want to get warning?")
 while True:
     root = tk.Tk()
     root.withdraw()
     result = subprocess.run(['WMIC', 'Path', 'Win32_Battery', 'Get', 'EstimatedChargeRemaining'], capture_output=True, text=True)
     lines = result.stdout.strip().split('\n')
     percent_line = next((line for line in lines if line.strip().isdigit()), None)
-    if int(percent_line) <= 30:
-        messagebox.showwarning("Battery warning", "Your battery is under 30")
-        break
-    time.sleep(30)
+    if percentage is not None:
+        if int(percent_line) <= int(percentage):
+            messagebox.showwarning("Battery warning", f"Your battery is under {percentage}")
+            break
+        time.sleep(30)
